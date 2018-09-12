e79b702dbdcb4f7c037630e7b3c80c4248bdb491
tomrambo <kittichai_tom007@hotmail.com>
2018-09-12 18:14:05 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -12,48 +12,56 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
-	$event = $events[1];
-	// Reply only when message sent is in 'text' format
-	if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// $text = $event['source']['userId'];
-			// $profile = $event['profile']['display_name'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+	foreach ($events['events'] as $event) {
 
-			if($event['message']['text'] == 'สวัสดี'){
-				$messages_response = 'สวัสดีต้องการให้ฉันช่วยอะไร ';
-			}else if($event['message']['text'] == 'หิวข้าว'){
-				$messages_response = 'กินสิ ';
-			}else if($event['message']['text'] == 'บุ๋ม' || $event['message']['text'] == 'บุ๋มบิ๋ม'){
-				$messages_response = 'ฉันรัก '.$event['message']['text'];
-			}else{
-				$messages_response = 'คุณชื่ออะไร ';
+		try {		
+		// Reply only when message sent is in 'text' format
+			if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+					// $text = $event['source']['userId'];
+					// $profile = $event['profile']['display_name'];
+					// Get replyToken
+					$replyToken = $event['replyToken'];
+
+					if($event['message']['text'] == 'สวัสดี'){
+						$messages_response = 'สวัสดีต้องการให้ฉันช่วยอะไร ';
+					}else if($event['message']['text'] == 'หิวข้าว'){
+						$messages_response = 'กินสิ ';
+					}else if($event['message']['text'] == 'บุ๋ม' || $event['message']['text'] == 'บุ๋มบิ๋ม'){
+						$messages_response = 'ฉันรัก '.$event['message']['text'];
+					}else{
+						$messages_response = 'คุณชื่ออะไร ';
+					}
+					// Build message to reply back
+					$messages = [
+						'type' => 'text',
+						'text' => $messages_response
+					];
+
 			}
-			// Build message to reply back
+			// Reply only when message sent is in 'sticker' format
+			else if($event['type'] == 'message' && $event['message']['type'] == 'sticker'){
+					if($event['message']['text'] ){
+						$stickerId = $event['message']['stickerId'];
+					}else{
+						$stickerId = 1;
+					}
+
+					$messages = [
+						'type' => 'sticker',
+						'text' => $stickerId
+					];
+			}else{
+					$messages = [
+						'type' => 'text',
+						'text' => json_encode($event)
+					];
+			}
+		} catch (Exception $e) {
 			$messages = [
 				'type' => 'text',
-				'text' => $messages_response
+				'text' => $e->getMessage()
 			];
-
-	}
-	// Reply only when message sent is in 'sticker' format
-	else if($event['type'] == 'message' && $event['message']['type'] == 'sticker'){
-			if($event['message']['text'] ){
-				$stickerId = $event['message']['stickerId'];
-			}else{
-				$stickerId = 1;
-			}
-
-			$messages = [
-				'type' => 'sticker',
-				'text' => $stickerId
-			];
-	}else{
-			$messages = [
-				'type' => 'text',
-				'text' => json_encode($event)
-			];
-	}
+		}
 
 		// Make a POST Request to Messaging API to reply to sender
 		$url = 'https://api.line.me/v2/bot/message/reply';
@@ -75,9 +83,8 @@ if (!is_null($events['events'])) {
 		curl_close($ch);
 
 		echo $result . "\r\n";
-	// foreach ($events['events'] as $event) {
 	
-	// }
+	}
 
 }
 echo "OK";
